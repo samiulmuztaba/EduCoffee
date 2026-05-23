9a0528bf5a63ef12e85bf76112741ad10645344b
Samiul Muztaba <samiulmuztaba101@gmail.com>
2026-05-23 13:13:49 +0600

    Update CORS settings to allow all origins

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://edu-coffee-seven.vercel.app', 'http://127.0.0.1:5500'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=["*"]
